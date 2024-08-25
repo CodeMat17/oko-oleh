8058b4e5783179ce83c6494c7ffb6118e705f619
Chukwu Matthew <codemat.biz@gmail.com>
2024-08-25 16:47:52 +0100

    updated

--- components/CondolenceCarousel.tsx
+++ components/CondolenceCarousel.tsx
@@ -1,13 +1,12 @@
-'use client'
+"use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import Image from "next/image";
 import { motion, useAnimation } from "framer-motion";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import CondolenceModal from "./CondolenceModal";
 import { Button } from "./ui/button";
-import { useInView } from "react-intersection-observer";
-import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
-
 
 interface CarouselProps {
   images: { src: string; title: string }[];
@@ -17,10 +16,10 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   const carouselRef = useRef<HTMLDivElement>(null);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-    const controls = useAnimation();
+  const controls = useAnimation();
   const { ref, inView } = useInView({
     triggerOnce: false,
-    threshold: 0.3
+    threshold: 0.3,
   });
 
   const scrollLeft = () => {
@@ -52,54 +51,56 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   }, [controls, inView]);
 
   return (
-    <div ref={ref} className='relative py-12 px-4  max-w-6xl mx-auto'>
-      <h2 className='text-center text-2xl font-semibold'>
-        CONDOLENCE MESSAGES
-      </h2>
-      <div
-        ref={carouselRef}
-        className='flex overflow-x-auto space-x-4 scrollbar-hide mt-8'>
-        {images.map((image, index) => (
-          <motion.div
-            key={index}
-            className='relative min-w-[300px] flex-shrink-0'
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}>
-            <div className='relative'>
-              <Image
-                src={image.src}
-                alt={image.title}
-                width={300}
-                height={200}
-                className='rounded-lg cursor-pointer shadow-lg border'
-                onClick={() => openModal(image.src)}
-              />
-              {/* <div className='absolute bottom-0 w-full bg-black bg-opacity-50 text-white p-2 text-center rounded-b-lg'>
+    <div className='bg-gray-50'>
+      <div ref={ref} className='relative py-12 px-4  max-w-6xl mx-auto'>
+        <h2 className='text-center text-2xl font-semibold'>
+          CONDOLENCE MESSAGES
+        </h2>
+        <div
+          ref={carouselRef}
+          className='flex overflow-x-auto space-x-4 scrollbar-hide mt-8'>
+          {images.map((image, index) => (
+            <motion.div
+              key={index}
+              className='relative min-w-[300px] flex-shrink-0'
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}>
+              <div className='relative'>
+                <Image
+                  src={image.src}
+                  alt={image.title}
+                  width={300}
+                  height={200}
+                  className='rounded-lg cursor-pointer shadow-lg border'
+                  onClick={() => openModal(image.src)}
+                />
+                {/* <div className='absolute bottom-0 w-full bg-black bg-opacity-50 text-white p-2 text-center rounded-b-lg'>
                 {image.title}
               </div> */}
-            </div>
-          </motion.div>
-        ))}
-      </div>
-      <div className='flex gap-4 justify-end my-4'>
-        <Button
-          onClick={scrollLeft}
-          size='icon'
-          variant='outline'
-          className='bg-gray-100'>
-          <ChevronLeftIcon />
-        </Button>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+        <div className='flex gap-4 justify-end my-4'>
+          <Button
+            onClick={scrollLeft}
+            size='icon'
+            variant='outline'
+            className='bg-gray-100'>
+            <ChevronLeftIcon />
+          </Button>
 
-        <Button
-          onClick={scrollRight}
-          size='icon'
-          variant='outline'
-          className='bg-gray-100'>
-          <ChevronRightIcon />
-        </Button>
+          <Button
+            onClick={scrollRight}
+            size='icon'
+            variant='outline'
+            className='bg-gray-100'>
+            <ChevronRightIcon />
+          </Button>
+        </div>
+        <CondolenceModal imageSrc={selectedImage} onClose={closeModal} />
       </div>
-      <CondolenceModal imageSrc={selectedImage} onClose={closeModal} />
     </div>
   );
 };
--- data/condolence.ts
+++ data/condolence.ts
@@ -1,8 +1,78 @@
 export const images = [
-  { src: "/oko.webp", title: "Image 1" },
-  { src: "/pdfs/condo.png", title: "Image 2" },
-  { src: "/pdfs/soft-lutions.png", title: "Image 3" },
-  { src: "/oko.webp", title: "Image 4" },
-  { src: "/oko.webp", title: "Image 5" },
-  // Add more images as needed
+  { src: "/pdfs/1fedsco.webp", title: "" },
+  { src: "/pdfs/2Ict_condolence[1]_00001.png", title: "Image 2" },
+  {
+    src: "/pdfs/3The Nigerian Society of Engineer1 condolence message.png",
+    title: "Image 3",
+  },
+  { src: "/pdfs/4TRIBUTE.png", title: "Image 4" },
+  { src: "/pdfs/5TRIBUTE2.png", title: "Image 5" },
+  {
+    src: "/pdfs/6WhatsApp Image 2024-08-05 at 19.36.49_7865a6c1.jpg",
+    title: "Image 6",
+  },
+  {
+    src: "/pdfs/7WhatsApp Image 2024-08-05 at 19.43.20_5c49eb94.jpg",
+    title: "Image 7",
+  },
+  { src: "/pdfs/8Scan_27_(1)_240805_193320[1].png", title: "Image 8" },
+  {
+    src: "/pdfs/9Prof_AKINOLA'S_tRIBUTE_240805_193359[1].png",
+    title: "Image 9",
+  },
+  {
+    src: "/pdfs/11Chief_Oko_Oleh_-_Condolence_Letter[25114]_240805_193430[1].png",
+    title: "Image 10",
+  },
+  { src: "/pdfs/12Condolence to Dr. Ole_.06-08-2024.png", title: "Image 11" },
+  {
+    src: "/pdfs/14Computer_Enginnering_Department_condolence_to_Ugon_240806_173320[1].png",
+    title: "Image 12",
+  },
+  { src: "/pdfs/15TRIBUTE2_240806_173546[1].png", title: "Image 13" },
+  { src: "/pdfs/16TRIBUTE_240806_173610[1].png", title: "Image 14" },
+  {
+    src: "/pdfs/17Computer_Enginnering_Department_condolence_to_Ugon_240806_181430[1].png",
+    title: "Image 15",
+  },
+  {
+    src: "/pdfs/19WhatsApp Image 2024-08-06 at 22.15.17_ff396cb4.jpg",
+    title: "Image 16",
+  },
+  { src: "/pdfs/20condolence_letter_from_soft-lutions.png", title: "Image 17" },
+  { src: "/pdfs/25Condolence_message_to_chairman-1.png", title: "Image 18" },
+  {
+    src: "/pdfs/21WhatsApp Image 2024-08-13 at 08.47.13_391093aa.jpg",
+    title: "Image 19",
+  },
+  {
+    src: "/pdfs/22WhatsApp Image 2024-08-13 at 08.47.15_a082462b.jpg",
+    title: "Image 20",
+  },
+  { src: "/pdfs/23CamScanner 08-13-2024 10.22.png", title: "Image 21" },
+  { src: "/pdfs/24TRIBUTE (1).png", title: "Image 4" },
+  { src: "/pdfs/26TRIBUTE TO UNCLE IMPERIAL.png", title: "Image 22" },
+  { src: "/pdfs/27CamScanner 08-16-2024 16.09.png", title: "Image 23" },
+  { src: "/pdfs/28CamScanner 08-16-2024 16.08.png", title: "Image 24" },
+  { src: "/pdfs/29Condolence_message_013451[1].png", title: "Image 25" },
+  { src: "/pdfs/30Condolence to Chief Oko Agbi Oleh.png", title: "Image 26" },
+  {
+    src: "/pdfs/31OLEH AN IMPERIAL MAN OF HISTORY_00001.png",
+    title: "Image 27",
+  },
+  {
+    src: "/pdfs/31OLEH AN IMPERIAL MAN OF HISTORY_00002.png",
+    title: "Image 28",
+  },
+  { src: "/pdfs/32CONDOLENCE FOR QUEEN OLEH.png", title: "Image 4" },
+  { src: "/pdfs/33Chief Agha Udu Agha.png", title: "Image 29" },
+  {
+    src: "/pdfs/34WhatsApp Image 2024-08-23 at 16.11.23_7258e791.jpg",
+    title: "Image 30",
+  },
+  {
+    src: "/pdfs/35WhatsApp Image 2024-08-23 at 16.11.24_0ef9e4af.jpg",
+    title: "Image 31",
+  },
+  { src: "/pdfs/36Mrs_Mary_Oko_Oleh_1[1][1].png", title: "Image 32" },
 ];
