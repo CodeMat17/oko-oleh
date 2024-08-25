4c7cdc71a342cf3389ff5dea9370e2ac18a3c4c1
Chukwu Matthew <codemat.biz@gmail.com>
2024-08-25 14:47:47 +0100

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,41 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "GOODBYE, OKO OLEH",
-  description: "This is the official website for the funeral service of Chief Hori Oko Agbi Oleh. Developed by his son-in-law, Engr. Chukwu Matthew.",
+  description:
+    "This is the official website for the funeral service of Chief Hori Oko Agbi Oleh. Developed by his son-in-law, Engr. Chukwu Matthew.",
+  twitter: {
+    card: "summary",
+    title: "GOODBYE, OKO OLEH",
+    description:
+      "This is the official website for the funeral service of Chief Hori Oko Agbi Oleh. Developed by his son-in-law, Engr. Chukwu Matthew.",
+  },
+  openGraph: {
+    title: "GOODBYE, OKO OLEH",
+    description:
+      "This is the official website for the funeral service of Chief Hori Oko Agbi Oleh. Developed by his son-in-law, Engr. Chukwu Matthew.",
+    url: "https://oko-oleh.vercel.app",
+    // siteName: "Portfolio website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/mctony17/image/upload/v1724592799/oko-oleh/oko-oleh.jpg",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://res.cloudinary.com/mctony17/image/upload/v1724592799/oko-oleh/oko-oleh.jpg",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+    // authors: ["Matthew Chukwu"],
+  },
 };
+
+
+// https://res.cloudinary.com/mctony17/image/upload/v1724592799/oko-oleh/oko-oleh.jpg
 
 export default function RootLayout({
   children,
